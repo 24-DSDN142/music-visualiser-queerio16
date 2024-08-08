9eb6484564d07b195d485ae359c23ab378b4a397
queerio16 <asrms2006@gmail.com>
2024-08-08 17:32:05 +1200

    m
    much progress very wow. i sorted out my background grid sizing and Ive started to figure out my sizing of different elements

--- music_starter.js
+++ music_starter.js
@@ -6,70 +6,65 @@ function draw_one_frame(words, vocal, drum, bass, other, counter) {
   rectMode(CENTER)
   textSize(24);
   
-  strokeWeight(2)
-  line(0,40,height,40);
-  line(0,80,height,80);
-  line(0,120,height,120);
-  line(0,160,height,160);
-  line(0,200,height,200);
-  line(0,240,height,240); 
-  line(0,280,height,280);
-  line(0,320,height,320);
-  line(0,360,height,360);
-  line(0,400,height,400);
-  line(0,440,height,440);
-  line(0,480,height,480);
-  line(0,520,height,520)
-  line(0,560,height,560)
 
-  line(40,0,40,width)
-  line(80,0,80,width)
+//grid
+  strokeWeight(2)
+  line(0,30,height,30)
+  line(0,60,height,60)
+  line(0,90,height,90)
+  line(0,120,height,120)
+  line(0,150,height,150)
+  line(0,180,height,180)
+  line(0,210,height,210)
+  line(0,240,height,240)
+  line(0,270,height,270)
+  line(0,300,height,300)
+  line(0,330,height,330)
+  line(0,360,height,360)
+  line(0,390,height,390)
+  line(0,420,height,420)
+  line(0,450,height,450)
+  line(0,480,height,480)
+  line(0,510,height,510)
+  line(0,540,height,540)
+  line(0,570,height,570)
+  
+  line(30,0,30,width)
+  line(60,0,60,width)
+  line(90,0,90,width)
   line(120,0,120,width)
-  line(160,0,160,width)
-  line(200,0,200,width)
+  line(150,0,150,width)
+  line(180,0,180,width)
+  line(210,0,210,width)
   line(240,0,240,width)
-  line(280,0,280,width)
-  line(320,0,320,width)
+  line(270,0,270,width)
+  line(300,0,300,width)
+  line(330,0,330,width)
   line(360,0,360,width)
-  line(400,0,400,width)
-  line(440,0,440,width)
+  line(390,0,390,width)
+  line(420,0,420,width)
+  line(450,0,450,width)
   line(480,0,480,width)
-  line(520,0,520,width)
-  line(560,0,560,width)
-   
+  line(510,0,510,width)
+  line(540,0,540,width)
+  line(570,0,570,width)
+
+//shapes
+  noStroke()
+  // triangle(40,50,90,70,70,20)
+  // triangle(140,540,300,510,208,420)
+  circle(66,352,50,50)
+  circle(54,103,60,60)
+
+//lights
+  //
+  stroke(0)
+  fill(66, 227, 245)
+  quad(30,30,width,450,width,height,220,height)
+  fill(245, 66, 221)
+  quad(570,30,360,height,0,height,0,500)
 
 }
 
-  //  let bar_spacing = height / 10;
-  //  let bar_height = width / 12;
-  //  let bar_pos_x = width / 2;
 
-// vocal bar is red
-  //  fill(200, 0, 0);
-  //  rect(bar_pos_x, height / 2 + 1 * bar_spacing, 4 * vocal, bar_height);
-  //  fill(0);
-  //  text("vocals", bar_pos_x, height / 2 + 1 * bar_spacing + 8);
- 
-  //  // drum bar is green
-  //  fill(0, 200, 0);
-  //  rect(bar_pos_x, height / 2 + 2 * bar_spacing, 4 * drum, bar_height);
-  //  fill(0);
-  //  text("drums", bar_pos_x, height / 2 + 2 * bar_spacing + 8);
- 
-  //  // bass bar is blue
-  //  fill(50, 50, 240);
-  //  rect(bar_pos_x, height / 2 + 3 * bar_spacing, 4 * bass, bar_height);
-  //  fill(0);
-  //  text("bass", bar_pos_x, height / 2 + 3 * bar_spacing + 8);
- 
-  //  // other bar is white
-  //  fill(200, 200, 200);
-  //  rect(bar_pos_x, height / 2 + 4 * bar_spacing, 4 * other, bar_height);
-  //  fill(0);
-  //  text("other", bar_pos_x, height / 2 + 4 * bar_spacing + 8);
-  //  fill(255, 255, 0);
- 
-  //  // display "words"
-  //  textAlign(CENTER);
-  //  textSize(vocal);
-  //  text(words, width/2, height/3);
+  
