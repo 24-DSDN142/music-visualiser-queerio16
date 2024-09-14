71b5ab895472612f5f0650b14569086090ccf77d
queerio16 <asrms2006@gmail.com>
2024-09-14 22:25:32 +1200

    Update music_starter.js
    ahhh

--- music_starter.js
+++ music_starter.js
@@ -59,16 +59,7 @@ function draw_one_frame(words, vocal, drum, bass, other, counter) {
   line(570,0,570,width)
 
 
-
-
-// for(let i = 0; i < 50; i+=5){
-    
-//   quad(54,20+i,82,47-i,60,65+i,30,30-i);
-//   scale(1.2)
-// }
-
-//  
-// code taken from mhf273 on p5.js editor with some modifications      https://editor.p5js.org/mhf273/sketches/rJp90Psob
+// SHAPES AND COLOURS
 push()
 stroke(0)
 translate(533, 183);
@@ -96,14 +87,12 @@ strokeWeight(17)
 
 // shapes amd coloursss
   noStroke()
-  fill(250, 209, 65)   //yellow
+  fill(250, 163, 0)
   push()
   scale(0.68)
   translate(154,54)
   triangle(192-50,128-50,249+17,92-62,247,157)
   pop()
-
-
 
   //black colour drop shadows
   fill(0)
@@ -147,11 +136,26 @@ strokeWeight(17)
 
   circle(56,352,93)
   fill(12, 178, 192)  //dark teal blue 
-  // arc(7,232,67,67,0+49,180+49)
   
+  push()
+  fill(244, 83, 138)
+  translate(340,-170)
+  circle(56,352,77)
+  pop()
+
   push()
   scale(0.9)
   translate(120,10)
+  arc(532,147,82,82,0-27,180-27)
+  pop()
+
+  push()
+  scale(1.2)
+  rotate(50)
+  fill(0)
+  translate(-50,-40)
+  arc(532+5,147+4,82,82,0-27,180-27)
+  fill(12, 178, 192)
   arc(532,147,82,82,0-27,180-27)
   pop()
 
@@ -184,8 +188,6 @@ translate(-325,-182)
   circle(558,373,43) //top
 pop()
 
-
-
   push()
   fill(244, 83, 138) //strawberry sour
   translate(600/2 + 478.8, 600/2 + 132.3) // i used chatgpt to get this because i lost the arc into the void and couldn't find it at all
@@ -213,26 +215,49 @@ push()
 pop()
  
 // code taken from mhf273 on p5.js editor with some modifications      https://editor.p5js.org/mhf273/sketches/rJp90Psob
-// push()
-// translate(213, 32);
-// rotate(146)
-// scale (0.2)
-// noFill()
-// strokeWeight(13)
-//   var y = 30;
+push()
+translate(213, 32);
+rotate(146)
+scale (0.2)
+noFill()
+strokeWeight(13)
+  var y = 30;
 	
-//   beginShape();	
-//   for (var i =0; i<10; i++) {
+  beginShape();	
+  for (var i =0; i<10; i++) {
 
-// 		vertex(i*50, y);
-// 		if(y == -50) {
-// 			y = 50;
-// 		} else {
-// 			y = -50;
-// 		}
-// 	}
-// 	endShape();
-//   pop()
+		vertex(i*50, y);
+		if(y == -50) {
+			y = 50;
+		} else {
+			y = -50;
+		}
+	}
+	endShape();
+  pop()
+
+
+  push()
+translate(537, 502);
+rotate(-10)
+scale(-1,-1)
+scale (0.4)
+noFill()
+strokeWeight(13)
+  var y = 30;
+	
+  beginShape();	
+  for (var i =0; i<10; i++) {
+
+		vertex(i*50, y);
+		if(y == -50) {
+			y = 50;
+		} else {
+			y = -50;
+		}
+	}
+	endShape();
+  pop()
 
 
   // code taken from mhf273 on p5.js editor with some modifications      https://editor.p5js.org/mhf273/sketches/rJp90Psob
@@ -260,20 +285,20 @@ strokeWeight(13)
 
 
 // light beams
-// if (other > 50) {
-//   fill(66, 227, 245);
-// } else {
-//   noFill();
-// }
-noFill()
-// noStroke();
+if (other > 50) {
+  fill(66, 227, 245);
+} else {
+  noFill();
+}
+
+noStroke();
  quad(30, 30, width, 450, width, height, 220, height);
 
-// if (other < 55) {
-//   fill(245, 66, 221);
-// } else {
-//   noFill();
-// }
+if (other < 55) {
+  fill(245, 66, 221);
+} else {
+  noFill();
+}
  quad(570, 30, 360, height, 0, height, 0, 500);
 
 
@@ -325,96 +350,96 @@ translate(300,300)
 scale(voc)
 fill(col)
 
-//   beginShape()
-//   vertex(224-7-300,280-26-300)
-//   vertex(254-7-300,279-26-300)
-//   vertex(250-7-300,313-26-300)
-//   vertex(264-7-300,312-26-300)
-//   vertex(265-7-300,295-26-300)
-//   vertex(294-7-300,294-26-300)
-//   vertex(284-7-300,371-26-300)
-//   vertex(250-7-300,372-26-300)
-//   vertex(252-7-300,352-26-300)
-//   vertex(237-7-300,354-26-300)
-//   vertex(235-7-300,370-26-300)
-//   vertex(208-7-300,374-26-300)
-//   vertex(224-7-300,280-26-300)
-//   endShape()
+  beginShape()
+  vertex(224-7-300,280-26-300)
+  vertex(254-7-300,279-26-300)
+  vertex(250-7-300,313-26-300)
+  vertex(264-7-300,312-26-300)
+  vertex(265-7-300,295-26-300)
+  vertex(294-7-300,294-26-300)
+  vertex(284-7-300,371-26-300)
+  vertex(250-7-300,372-26-300)
+  vertex(252-7-300,352-26-300)
+  vertex(237-7-300,354-26-300)
+  vertex(235-7-300,370-26-300)
+  vertex(208-7-300,374-26-300)
+  vertex(224-7-300,280-26-300)
+  endShape()
 
 
-// //W
-//   beginShape()
-//   vertex(128+6-7-300,373-26-300)
-//   vertex(110+6-7-300,296-26-300)
-//   vertex(146+6-7-300,288-26-300)
-//   vertex(150+6-7-300,307-26-300)
-//   vertex(167+6-7-300,288-26-300)
-//   vertex(173+6-7-300,318-26-300)
-//   vertex(187+6-7-300,299-26-300)
-//   vertex(222+6-7-300,301-26-300)
-//   vertex(171+6-7-300,380-26-300)
-//   vertex(157+6-7-300,348-26-300)
-//   vertex(128+6-7-300,373-26-300)
-//   endShape()
+//W
+  beginShape()
+  vertex(128+6-7-300,373-26-300)
+  vertex(110+6-7-300,296-26-300)
+  vertex(146+6-7-300,288-26-300)
+  vertex(150+6-7-300,307-26-300)
+  vertex(167+6-7-300,288-26-300)
+  vertex(173+6-7-300,318-26-300)
+  vertex(187+6-7-300,299-26-300)
+  vertex(222+6-7-300,301-26-300)
+  vertex(171+6-7-300,380-26-300)
+  vertex(157+6-7-300,348-26-300)
+  vertex(128+6-7-300,373-26-300)
+  endShape()
 
 
-// //M
-//   beginShape()
-//   vertex(367-7-300,288-26-300)
-//   vertex(386-7-300,317-26-300)
-//   vertex(421-7-300,284-26-300)
-//   vertex(424-7-300,363-26-300)
-//   vertex(398-7-300,364-26-300)
-//   vertex(396-7-300,343-26-300)
-//   vertex(378-7-300,367-26-300)
-//   vertex(369-7-300,342-26-300)
-//   vertex(362-7-300,366-26-300)
-//   vertex(340-7-300,365-26-300)
-//   vertex(367-7-300,288-26-300)
-//   endShape()
+//M
+  beginShape()
+  vertex(367-7-300,288-26-300)
+  vertex(386-7-300,317-26-300)
+  vertex(421-7-300,284-26-300)
+  vertex(424-7-300,363-26-300)
+  vertex(398-7-300,364-26-300)
+  vertex(396-7-300,343-26-300)
+  vertex(378-7-300,367-26-300)
+  vertex(369-7-300,342-26-300)
+  vertex(362-7-300,366-26-300)
+  vertex(340-7-300,365-26-300)
+  vertex(367-7-300,288-26-300)
+  endShape()
 
 
-// //A
-//   beginShape()
-//   vertex(264-7-300,385-26-300)
-//   vertex(327-7-300,274-26-300)
-//   vertex(345-7-300,376-26-300)
-//   vertex(321-7-300,382-26-300)
-//   vertex(319-7-300,370-26-300)
-//   vertex(298-7-300,371-26-300)
-//   vertex(291-7-300,381-26-300)
-//   vertex(264-7-300,384-26-300)
-//   vertex(307-7-300,352-26-300)
-//   vertex(321-7-300,352-26-300)
-//   vertex(317-7-300,332-26-300)
-//   vertex(307-7-300,352-26-300)
-//   endShape()
+//A
+  beginShape()
+  vertex(264-7-300,385-26-300)
+  vertex(327-7-300,274-26-300)
+  vertex(345-7-300,376-26-300)
+  vertex(321-7-300,382-26-300)
+  vertex(319-7-300,370-26-300)
+  vertex(298-7-300,371-26-300)
+  vertex(291-7-300,381-26-300)
+  vertex(264-7-300,384-26-300)
+  vertex(307-7-300,352-26-300)
+  vertex(321-7-300,352-26-300)
+  vertex(317-7-300,332-26-300)
+  vertex(307-7-300,352-26-300)
+  endShape()
 
-// // !!!
-//   beginShape()
-//   vertex(461-7-300,268-26-300)
-//   vertex(498-7-300,267-26-300)
-//   vertex(464-7-300,327-26-300)
-//   vertex(454-7-300,328-26-300)
-//   vertex(461-7-300,268-26-300)
-//   endShape()
+// !!!
+  beginShape()
+  vertex(461-7-300,268-26-300)
+  vertex(498-7-300,267-26-300)
+  vertex(464-7-300,327-26-300)
+  vertex(454-7-300,328-26-300)
+  vertex(461-7-300,268-26-300)
+  endShape()
 
-//   beginShape()
-//   vertex(460-7-300,330-26-300)
-//   vertex(462-7-300,344-26-300)
-//   vertex(478-7-300,343-26-300)
-//   vertex(463-7-300,356-26-300)
-//   vertex(464-7-300,374-26-300)
-//   vertex(454-7-300,364-26-300)
-//   vertex(428-7-300,373-26-300)
-//   vertex(440-7-300,355-26-300)
-//   vertex(428-7-300,343-26-300)
-//   vertex(448-7-300,344-26-300)
-//   vertex(460-7-300,330-26-300)
-//   vertex(460-7-300,330-26-300)
-//   endShape()
+  beginShape()
+  vertex(460-7-300,330-26-300)
+  vertex(462-7-300,344-26-300)
+  vertex(478-7-300,343-26-300)
+  vertex(463-7-300,356-26-300)
+  vertex(464-7-300,374-26-300)
+  vertex(454-7-300,364-26-300)
+  vertex(428-7-300,373-26-300)
+  vertex(440-7-300,355-26-300)
+  vertex(428-7-300,343-26-300)
+  vertex(448-7-300,344-26-300)
+  vertex(460-7-300,330-26-300)
+  vertex(460-7-300,330-26-300)
+  endShape()
 
-//   pop()
+  pop()
 
 
 }
